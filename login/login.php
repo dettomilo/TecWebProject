f958561ea47765deb95abcc1ff5dc1175e2f9afa
SerAlbi <alberto.ser11@gmail.com>
2016-12-27 16:27:46 +0100

    Added first bootstrap UI elemets

--- login/login.php
+++ login/login.php
@@ -22,9 +22,7 @@
 
   <body>
     <main>
-      <section id="content">
-        <h1>Titolo di esempio</h1>
-        <p>Descrizione</p>
+      <section id="content" class="container-fluid">
 
         <?php
         //Includo il file esterno relativo le funzioni di login.
@@ -38,7 +36,9 @@
         if (isset($_POST['email'], $_POST['pw'])) {
           //Controllo che i valori non siano nulli
           if($_POST['email'] == NULL || $_POST['pw'] == NULL) {
-            echo "<p>I valori non possono essere nulli</p>";
+            echo "
+            <p class='text-center'>I valori non possono essere nulli</p>
+            ";
           } else {
             //Includo il file esterno relativo la connessione al database.
             require("db_connect.php");
@@ -48,22 +48,45 @@
             //Eseguo un tentativo di login.
             if (login($email, $password, $mysqli) == true) {
               //Login eseguito.
-              echo "<p>Success: You have been logged in</p>";
+              echo "<p class='text-center'>Success: You have been logged in</p>";
             } else {
               //Login fallito.
-              echo "<p>Login failed</p>";
+              echo "<p class='text-center'>Login failed</p>";
             }
           }
         }
         ?>
 
-        <form action="login.php" method="post" name="login_form">
-          <label for="email">Email</label>
-          <input type="email" name="email" id="email" autocomplete="on"/><br/>
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password" autocomplete="off"/><br/>
-          <input type="button" value="Login" onclick="formhash(this.form, this.form.password);"/>
-        </form>
+        <div class="modal-dialog">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <h1 class="text-center">Smart Unibo</h1>
+            </div>
+
+            <form action="login.php" method="post" name="login_form">
+            <div class="modal-body">
+
+              <div class="form-group">
+                <input type="email" name="email" id="email" autocomplete="on" placeholder="email istituzionale" class="form-control input-lg"/><br/>
+                <label for="email" class="sr-only">Email</label>
+              </div>
+
+              <div class="form-group">
+                <input type="password" name="password" id="password" autocomplete="off" placeholder="password" class="form-control input-lg"/><br/>
+                <label for="password" class="sr-only">Password</label>
+              </div>
+
+              <div class="form-group">
+                <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" class="btn btn-block btn-lg btn-danger"/>
+              </div>
+
+            </div>
+            </form>
+
+          </div>
+        </div>
+
       </section>
     </main>
   </body>
