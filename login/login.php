6799f759bc7390832b5259fedb80208e35537a9f
SerAlbi <alberto.ser11@gmail.com>
2016-12-27 15:43:30 +0100

    Integrated Bootstrap CDN within login.php
    Removed login.html

--- login/login.php
+++ login/login.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="it">
+
   <head>
-    <meta charset="utf-8">
     <title>Smart Unibo</title>
+
+    <!-- meta informations -->
+    <meta charset="UTF-8">
+    <!-- to ensure proper zooming and rendering on mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     <script type="text/javascript" src="sha512.js"></script>
     <script type="text/javascript" src="forms.js"></script>
   </head>
+
   <body>
     <main>
       <section id="content">
