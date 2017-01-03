25fc8b0939063e4f7ae138a9c527540fb63161f4
dettomilo <edettori7@gmail.com>
2017-01-03 16:27:12 +0100

    Added header in home page

--- home/home.php
+++ home/home.php
@@ -17,7 +17,43 @@
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	    <!-- bootstrap CSS override -->
-	    <link rel="stylesheet" type="text/css" href="home.css" media="screen" />
+	    <link rel="stylesheet" type="text/css" href="home.css" media="screen"/>
   	</head>
+
+  	<body>
+  		<main>
+  			<section id=content class="container-fluid">
+	  			<header class="header">
+	  				<div>
+	  					<a href="#">
+	            			<img src="alma-mater-studiorum.png" alt="Alma Mater Studiorum A.D. 1088">
+	            			<img class="unibo" src="universita-di-bologna.png" alt="Università di Bologna">
+            			</a>
+	  				</div>
+	  			</header>
+
+  				<nav class="navbar navbar-default mm-margin">
+				    <div class="container">
+				    	<div class="row">
+				        	<div class="col-md-12">
+				              <ul class="nav navbar-nav">
+				                  <li role="presentation" class="active"><a href="#">Home</a></li>
+				                  <li role="presentation"><a href="#">News</a></li>
+				                  <li role="presentation"><a href="#">Servizi</a></li>
+				                  <li role="presentation"><a href="#">Carriera</a></li>
+				              </ul>
+				            </div>
+				    	</div>
+				    </div>
+				</nav>
+
+				<div class="container">
+					<h1>Benvenuto su Smart Unibo!</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p><a class="btn btn-primary btn-lg" href="#" role="button">Clicca qui!</a></p>
+			    </div>
+  			</section>
+  		</main>
+  	</body>
 
 </html>
