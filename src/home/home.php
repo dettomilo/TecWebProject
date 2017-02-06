5ce74b8fe25ed32821cdfe46d8329ab77496de9b
SerAlbi <alberto.ser11@gmail.com>
2017-02-06 13:20:33 +0100

    Demo navbar with dropdown menu

--- src/home/home.php
+++ src/home/home.php
@@ -42,19 +42,47 @@
 	  				</div>
 	  			</header>
 
-					<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-					<div class="container">
-						<div class="row">
-								<div class="col-md-12">
-										<ul class="nav navbar-nav">
-												<li role="presentation" class="active"><a href="#">News</a></li>
-												<li role="presentation"><a href="#">Servizi</a></li>
-												<li role="presentation"><a href="#">Carriera</a></li>
-										</ul>
-									</div>
-						</div>
-					</div>
-			    </nav>
+			<!-- NAV BAR -->
+			<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Apri o Chiudi navigazione</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Smart Unibo</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+            <ul class="nav navbar-nav">
+
+							<li class="active"><a href="#">News</a></li>
+              <li><a href="#">Servizi</a></li>
+              <li><a href="#">Carriera</a></li>
+<!--
+							<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Static top</a></li>
+              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            </ul>
+					-->
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
 
 				<div class="jumbotron">
 					<div class="container">
