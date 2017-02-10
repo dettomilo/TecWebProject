a6e1fc74002b9e3b99767c00ab7ce287f1df5844
dettomilo <edettori7@gmail.com>
2017-02-10 15:42:30 +0100

    Updated sample db

--- src/events.sql
+++ src/events.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 08, 2017 alle 17:00
+-- Creato il: Feb 10, 2017 alle 15:41
 -- Versione del server: 10.1.16-MariaDB
 -- Versione PHP: 5.6.24
 
@@ -23,24 +23,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `evenement`
+-- Struttura della tabella `events`
 --
 
-CREATE TABLE `evenement` (
+CREATE TABLE `events` (
   `id` int(5) NOT NULL,
   `title` varchar(30) NOT NULL,
-  `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `allDay` int(1) NOT NULL,
   `url` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dump dei dati per la tabella `evenement`
+-- Dump dei dati per la tabella `events`
 --
 
-INSERT INTO `evenement` (`id`, `title`, `start`, `end`, `url`) VALUES
-(1, 'Aperitivo', '2017-02-09 18:00:00', '2017-02-09 19:00:00', 'https://google.it'),
-(2, 'Esame', '2017-02-09 08:00:00', '2017-02-08 10:00:00', 'http://amazon.it');
+INSERT INTO `events` (`id`, `title`, `start`, `end`, `allDay`, `url`) VALUES
+(1, 'Aperitivo', '2017-02-10 19:00:00', '2017-02-10 20:00:00', 0, ''),
+(2, 'Scadenza Tassa', '2017-02-09 00:00:00', '2017-02-09 00:00:00', 1, ''),
+(3, 'Esame', '2017-02-11 09:00:00', '2017-02-11 14:00:00', 0, ''),
+(4, 'Evento Sprite', '2017-02-07 00:00:00', '2017-02-07 00:00:00', 1, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
