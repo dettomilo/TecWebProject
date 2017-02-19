3dfbe500c889eec766a519c993226d241fade868
dettomilo <edettori7@gmail.com>
2017-02-19 11:35:42 +0100

    Implemented function in notification

--- src/home/notifications/notifications_functions.php
+++ src/home/notifications/notifications_functions.php
@@ -1,33 +1,27 @@
 <?php
 /*
-* Questa funzione restituisce le notizie più recenti, in relazione ai parametri specificati:
-* - categoria delle notizie desiderate (0 = ateneo, 1 = corso)
-* - numero di notizie richieste
-* Viene restituito un array di array associativi, contenenti le varie notizie e, per ognuna
-* di esse, le informazioni di interesse.
-* Link di riferimento sulla soluzione adottata:
+* This function return an array with all the current notifications, ordered by date.
+* Based on:
 * - http://php.net/manual/it/mysqli-result.fetch-array.php
 */
-function getNews($isCorso, $numNews, $mysqli) {
-  if ($stmt = $mysqli->prepare("SELECT IdNotizia, t.Tipo, DATE_FORMAT(DATE(DataOra), '%d/%m/%Y') as Data, Titolo, Sommario, Testo, Immagine
-  FROM notizie AS n INNER JOIN tipi_notizie AS t ON n.Tipo = t.IdTipo
-  WHERE IsCorso = ? ORDER BY DataOra DESC LIMIT ?")) {
-    $stmt->bind_param('ii', $isCorso, $numNews); //Eseguo il binding dei parametri
-    $stmt->execute(); //Eseguo la query appena creata
-    $result = $stmt->get_result(); //Ottengo il risultato della query
-    $temp = array();
-    $array = array();
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) { //Per ogni notizia...
-      $temp["Tipo"] = $row["Tipo"];
-      $temp["Data"] = $row["Data"];
-      $temp["Titolo"] = $row["Titolo"];
-      $temp["Sommario"] = $row["Sommario"];
-      $temp["Testo"] = $row["Testo"];
-      $temp["Immagine"] = $row["Immagine"];
-      $array[$row['IdNotizia']] = $temp; //... Memorizzo i dati in un nuovo array associativo
-      unset($temp);
-    }
-    return $array;
-  }
+function getNews($mysqli) {
+	if ($stmt = $mysqli->prepare("SELECT IdNotifica, Titolo, Url, t.Tipo, DataOra, t.Icona
+								  FROM notifiche AS n INNER JOIN tipi_notifiche AS t ON n.Tipo = t.Tipo
+								  ORDER BY DataOra")) {
+		$stmt->execute(); // Execute the query
+		$result = $stmt->get_result(); // Getting query result
+		$temp = array();
+		$array = array();
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)) { // For each notification...
+			$temp["Titolo"] = $row["Titolo"];
+			$temp["DataOra"] = $row["DataOra"];
+			$temp["Url"] = $row["Url"];
+			$temp["Tipo"] = $row["Tipo"];
+			$temp["Icona"] = $row["Icona"];
+			$array[$row['IdNotifica']] = $temp; //... we put the data in the array
+			unset($temp);
+		}
+		return $array;
+	}
 }
 ?>
