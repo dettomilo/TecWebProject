21997e6784dd90116da567a3106fa11d0b07c3fb
Giacomo Frisoni <giacomofrisoni@gmail.com>
2017-02-20 02:16:59 +0100

    Fixed calendar colors and db privileges

--- src/home/calendar/events.php
+++ src/home/calendar/events.php
@@ -10,28 +10,26 @@ $request = "SELECT *
 
 // connection to the database
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=smartunibo', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=smartunibo', 'admin_user', 'KWvHbgcLtCimIMBg');
 } catch(Exception $ex) {
 	exit('Unable to connect to database.');
 }
+
  // Execute the query
 $result = $bdd->query($request) or die(print_r($bdd->errorInfo()));
 
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
 	$event = array();
 	$event['id'] = $row['IdEvento'];
 	$event['title'] = $row['Titolo'];
 	$event['start'] = $row['DataOraInizio'];
 	$event['end'] = $row['DataOraFine'];
-	$event['color'] = $row['Colore'];
+	$event['color'] = "#" . $row['Colore'];
 	if ($row['GiornoIntero'] == 0) {
 		$event['allDay'] = false;
-
 	} else {
 		$event['allDay'] = true;
 	}
-
 	// Merge the event array into the return array
 	array_push($json, $event);
 }
@@ -42,7 +40,6 @@ $request = "SELECT * FROM lezioni";
 $result = $bdd->query($request) or die(print_r($bdd->errorInfo()));
 
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
 	$event = array();
 	$event['id'] = $row['IdLezione'];
 	$event['title'] = $row['NomeMateria'];
@@ -56,8 +53,5 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 }
 
 // sending the encoded result to success page
-//$json = $result->fetchAll(PDO::FETCH_ASSOC);
-
 echo json_encode($json);
-
 ?>
