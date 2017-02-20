0d45af1c02da6ac7b64c2c66040f76d5d71006fd
SerAlbi <alberto.ser11@gmail.com>
2017-02-20 21:40:51 +0100

    PROJECT COMPLETED! :)

--- src/home/home.php
+++ src/home/home.php
@@ -257,7 +257,7 @@
 
 							            <div class="panel-body">
 														<div class="row">
-															<div class="col-md-11 col-sm-11 col-xs-10">
+															<div class="col-md-11 col-sm-11 col-xs-9">
 															';
 															if (!is_null($n["Sommario"])) {
 																echo $n["Sommario"];
@@ -266,7 +266,7 @@
 															}
 														echo '
 															</div>
-																	<button type="button" class="col-md-1 col-sm-1 col-xs-2 btn btn-info btn-md pull-left" data-toggle="modal" data-target="#'.$currentNewsId.'">Leggi</button>
+																	<button type="button" class="col-md-1 col-sm-1 col-xs-3 btn btn-info btn-md pull-left" data-toggle="modal" data-target="#'.$currentNewsId.'">Leggi</button>
 														</div>
 							            </div>
 
